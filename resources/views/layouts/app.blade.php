a91f555062efeab83817b58adda02532d7c1551a
bprayush <bprayush@yahoo.com>
2019-12-23 17:16:32 +0545

    Finished importing layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
             integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <!-- Font Awesome 5.11-->
-        <link rel="stylesheet" href="{{ asset('public/css/all.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/all.min.css') }}">
         <!-- Our Custom CSS-->
         <link rel="stylesheet" href="{{ asset('css/main.css') }}">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
