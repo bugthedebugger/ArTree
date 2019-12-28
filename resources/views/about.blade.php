7e1e602098b02ce260da22c2b5528c3d6bf36151
Aakash Raj Dahal <akashrajdahal@gmail.com>
2019-12-28 22:11:52 +0545

    About Page completed

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,6 +2,123 @@
 
 @section('body')
 
-hello
+
+<div class="container">
+    <div class="page">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="aboutHead">
+                    <h1>About us</h1>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="aboutTagline">
+                    <i class="fas fa-quote-left"></i>
+                    <p>
+                        ArtTree projects explore a multitude of identities, cultures, and dynamics with strategic grassroots interventions and a goal of shaping emerging communities. 
+                    </p>
+                </div>
+            </div>
+        </div>
+        <br>
+        <p>
+                The members of ArTree Nepal practice contemporary art with the belief that art has deep roots in social practice - that creativity can foster meaningful dialogue. Through interdisciplinary practices that focus on critical subject matter, ArTree Nepal strives to create artworks that have social significance and utility.
+                <br><br>ArtTree projects explore a multitude of identities, cultures, and dynamics with strategic grassroots interventions and a goal of shaping emerging communities. ArTree members approach their work by consciously prioritizing a balance of methodology, materiality, concept and aesthetics to forward their mission to engage audiences and urge them to reflect on social issues.
+                <br><br>ArTree also aims to further Nepali contemporary art practices by embodying its values of experimentation and self-reflection, as individual artists and as a collective movement.
+        </p>
+        <br><br>
+        <center>
+            <h1>Meet the Team</h1>
+        </center><br>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="team">
+                    <center>
+                        <img src="images/01teem.png" class="teamPic"><br><br>
+                        <h4>Hitman Gurung</h4>
+                        <span class="socialIcons">
+                            <i class="fab fa-facebook"></i>&nbsp;
+                            <i class="fab fa-twitter"></i>&nbsp;
+                            <i class="fab fa-instagram"></i>&nbsp;
+                        </span><br>
+                        <a href="#" class="bioLink">View Biography</a>
+                    </center>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="team">
+                    <center>
+                        <img src="images/02teem.png" class="teamPic"><br><br>
+                        <h4>Sheelasha Raj Bhandari</h4>
+                        <span class="socialIcons">
+                            <i class="fab fa-facebook"></i>&nbsp;
+                            <i class="fab fa-twitter"></i>&nbsp;
+                            <i class="fab fa-instagram"></i>&nbsp;
+                        </span><br>
+                        <a href="#" class="bioLink">View Biography</a>
+                    </center>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="team">
+                    <center>
+                        <img src="images/03teem.png" class="teamPic"><br><br>
+                        <h4>Mekh Limbu</h4>
+                        <span class="socialIcons">
+                            <i class="fab fa-facebook"></i>&nbsp;
+                            <i class="fab fa-twitter"></i>&nbsp;
+                            <i class="fab fa-instagram"></i>&nbsp;
+                        </span><br>
+                        <a href="#" class="bioLink">View Biography</a>
+                    </center>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="team">
+                    <center>
+                        <img src="images/04teem.png" class="teamPic"><br><br>
+                        <h4>Subas Tamang</h4>
+                        <span class="socialIcons">
+                            <i class="fab fa-facebook"></i>&nbsp;
+                            <i class="fab fa-twitter"></i>&nbsp;
+                            <i class="fab fa-instagram"></i>&nbsp;
+                        </span><br>
+                        <a href="#" class="bioLink">View Biography</a>
+                    </center>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="team">
+                    <center>
+                        <img src="images/05teem.png" class="teamPic"><br><br>
+                        <h4>Lavkant Chaudhary</h4>
+                        <span class="socialIcons">
+                            <i class="fab fa-facebook"></i>&nbsp;
+                            <i class="fab fa-twitter"></i>&nbsp;
+                            <i class="fab fa-instagram"></i>&nbsp;
+                        </span><br>
+                        <a href="#" class="bioLink">View Biography</a>
+                    </center>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="team">
+                    <center>
+                        <img src="images/06teem.png" class="teamPic"><br><br>
+                        <h4>Bikash Shrestha</h4>
+                        <span class="socialIcons">
+                            <i class="fab fa-facebook"></i>&nbsp;
+                            <i class="fab fa-twitter"></i>&nbsp;
+                            <i class="fab fa-instagram"></i>&nbsp;
+                        </span><br>
+                        <a href="#" class="bioLink">View Biography</a>
+                    </center>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
