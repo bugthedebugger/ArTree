453df94c70bb816a73bea8cedff3ae4935028f70
Aakash Raj Dahal <akashrajdahal@gmail.com>
2019-12-30 23:09:45 +0545

    Added Projects Page

---
+++ resources/views/projects.blade.php
@@ -0,0 +1,163 @@
+@extends('layouts.artree')
+
+@section('body')
+
+<div class="container">
+    <div class="page">
+        <h1>Projects from 2019</h1><br>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top" src="images/eye2.png" alt="Card image">
+                    <div class="card-body">
+                        <p class="card-title">
+                            <i class="fal fa-calendar"></i>&nbsp; 11th Sep 2019, Monday
+                        </p>
+                        <p class="card-text">
+                            <strong>Some cool Title</strong> <br>
+                            ... and African countries. According to the International Organization of Migration
+                            (IOM) for the only thing that matters is the fact that...
+                        </p>
+                        <a href="#" class="btn btn-artee">Read More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top" src="images/eye.png" alt="Card image">
+                    <div class="card-body">
+                        <p class="card-title">
+                            <i class="fal fa-calendar"></i>&nbsp; 11th Sep 2019, Monday
+                        </p>
+                        <p class="card-text">
+                            <strong>Some cool Title</strong> <br>
+                            ... and African countries. According to the International Organization of Migration
+                            (IOM) for the only thing that matters is the fact that...
+                        </p>
+                        <a href="#" class="btn btn-artee">Read More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top" src="images/eye2.png" alt="Card image">
+                    <div class="card-body">
+                        <p class="card-title">
+                            <i class="fal fa-calendar"></i>&nbsp; 11th Sep 2019, Monday
+                        </p>
+                        <p class="card-text">
+                            <strong>Some cool Title</strong> <br>
+                            ... and African countries. According to the International Organization of Migration
+                            (IOM) for the only thing that matters is the fact that...
+                        </p>
+                        <a href="#" class="btn btn-artee">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div><br>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top" src="images/eye2.png" alt="Card image">
+                    <div class="card-body">
+                        <p class="card-title">
+                            <i class="fal fa-calendar"></i>&nbsp; 11th Sep 2019, Monday
+                        </p>
+                        <p class="card-text">
+                            <strong>Some cool Title</strong> <br>
+                            ... and African countries. According to the International Organization of Migration
+                            (IOM) for the only thing that matters is the fact that...
+                        </p>
+                        <a href="#" class="btn btn-artee">Read More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top" src="images/eye.png" alt="Card image">
+                    <div class="card-body">
+                        <p class="card-title">
+                            <i class="fal fa-calendar"></i>&nbsp; 11th Sep 2019, Monday
+                        </p>
+                        <p class="card-text">
+                            <strong>Some cool Title</strong> <br>
+                            ... and African countries. According to the International Organization of Migration
+                            (IOM) for the only thing that matters is the fact that...
+                        </p>
+                        <a href="#" class="btn btn-artee">Read More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top" src="images/eye2.png" alt="Card image">
+                    <div class="card-body">
+                        <p class="card-title">
+                            <i class="fal fa-calendar"></i>&nbsp; 11th Sep 2019, Monday
+                        </p>
+                        <p class="card-text">
+                            <strong>Some cool Title</strong> <br>
+                            ... and African countries. According to the International Organization of Migration
+                            (IOM) for the only thing that matters is the fact that...
+                        </p>
+                        <a href="#" class="btn btn-artee">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br><br>
+        <h1>Projects from 2018</h1><br>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top" src="images/eye2.png" alt="Card image">
+                    <div class="card-body">
+                        <p class="card-title">
+                            <i class="fal fa-calendar"></i>&nbsp; 11th Sep 2019, Monday
+                        </p>
+                        <p class="card-text">
+                            <strong>Some cool Title</strong> <br>
+                            ... and African countries. According to the International Organization of Migration
+                            (IOM) for the only thing that matters is the fact that...
+                        </p>
+                        <a href="#" class="btn btn-artee">Read More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top" src="images/eye.png" alt="Card image">
+                    <div class="card-body">
+                        <p class="card-title">
+                            <i class="fal fa-calendar"></i>&nbsp; 11th Sep 2019, Monday
+                        </p>
+                        <p class="card-text">
+                            <strong>Some cool Title</strong> <br>
+                            ... and African countries. According to the International Organization of Migration
+                            (IOM) for the only thing that matters is the fact that...
+                        </p>
+                        <a href="#" class="btn btn-artee">Read More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top" src="images/eye2.png" alt="Card image">
+                    <div class="card-body">
+                        <p class="card-title">
+                            <i class="fal fa-calendar"></i>&nbsp; 11th Sep 2019, Monday
+                        </p>
+                        <p class="card-text">
+                            <strong>Some cool Title</strong> <br>
+                            ... and African countries. According to the International Organization of Migration
+                            (IOM) for the only thing that matters is the fact that...
+                        </p>
+                        <a href="#" class="btn btn-artee">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
