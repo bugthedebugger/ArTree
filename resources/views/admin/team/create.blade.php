8d1b5129dd5279c491327470f039cf41dd229335
bprayush <bprayush@yahoo.com>
2019-12-31 23:37:54 +0545

    working on team

--- resources/views/admin/team/create.blade.php
+++ resources/views/admin/team/create.blade.php
@@ -23,11 +23,102 @@ Add Team Member
                     </label>
                     <div class="col-sm-10">
                         <div class="custom-file @error('photo') is-invalid @enderror">
-                            <input name="logo" onchange="changed(this)" type="file" class="custom-file-input" id="inputGroupFile01"
-                            aria-describedby="inputGroupFileAddon01">
+                            <input name="photo" onchange="changed(this)" type="file" class="custom-file-input" id="inputGroupFile01"
+                            aria-describedby="inputGroupFileAddon01" required>
                             <label id="fileLabel" class="custom-file-label" for="inputGroupFile01">Choose file</label>
                         </div>
                         @error('photo')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="name" class="col-sm-2 col-form-label font-weight-bold">
+                        Name
+                    </label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name" placeholder="e.g. Clark Kent" required>
+                        @error('name')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="phone" class="col-sm-2 col-form-label font-weight-bold">
+                        Phone
+                    </label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" id="phone" placeholder="e.g. +977 1234567890" required>
+                        @error('phone')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="phone" class="col-sm-2 col-form-label font-weight-bold">
+                        Email
+                    </label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control @error('email') is-invalid @enderror" name="email" id="email" placeholder="e.g. artreenepal@gmail.com" required>
+                        @error('email')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="name" class="col-sm-2 col-form-label font-weight-bold">
+                        Facebook
+                    </label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control @error('facebook') is-invalid @enderror" name="facebook" id="facebook" placeholder="e.g. URL">
+                        @error('facebook')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="instagram" class="col-sm-2 col-form-label font-weight-bold">
+                        Instagram
+                    </label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control @error('instagram') is-invalid @enderror" name="instagram" id="instagram" placeholder="e.g. URL">
+                        @error('instagram')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="instagram" class="col-sm-2 col-form-label font-weight-bold">
+                        YouTube
+                    </label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control @error('youtube') is-invalid @enderror" name="youtube" id="youtube" placeholder="e.g. URL">
+                        @error('youtube')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="instagram" class="col-sm-2 col-form-label font-weight-bold">
+                        Twitter
+                    </label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control @error('twitter') is-invalid @enderror" name="twitter" id="twitter" placeholder="e.g. URL">
+                        @error('twitter')
                             <div class="invalid-feedback">
                                 {{ $message }}
                             </div>
@@ -41,4 +132,15 @@ Add Team Member
         </form>
     </div>
 </div>
+@endsection
+
+@section('scripts')
+<script>
+    function changed(e) {
+        var fileName = document.getElementById("inputGroupFile01").files[0].name;
+        console.log(fileName);
+        var nextSibling = document.getElementById("fileLabel");
+        nextSibling.innerText = fileName;
+    }
+</script>
 @endsection
