84e1b86231d648022008f800031d9ef4e5c70651
bprayush <bprayush@yahoo.com>
2020-01-01 14:54:59 +0545

    Fixed hidden posts being displayed

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -8,10 +8,13 @@ use App\Models\Project;
 class LandingPageController extends Controller
 {
     public function index() {
-        $featuredProject = Project::orderBy('project_date', 'desc')->first();
-        $projects = Project::orderBy('project_date', 'desc')->take(4)->get();
+        $featuredProject = Project::where('hidden', false)->orderBy('project_date', 'desc')->first();
+        $projects = Project::where('hidden', false)->orderBy('project_date', 'desc')->take(4)->get();
         $latestEvent = Project::orderBy('project_date', 'desc')
-                                ->where('event', true)
+                                ->where([
+                                    ['event', '=', true],
+                                    ['hidden', '=', false]
+                                ])
                                 ->first();
         return view('index')
                 ->with('featuredProject', $featuredProject)
