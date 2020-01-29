5dbb7cb7d2d596a57d5f31b96cb37ebab8b628ad
Aakash Raj Dahal <akashrajdahal@gmail.com>
2020-01-29 11:28:15 +0545

    fixes

--- resources/views/bio/index.blade.php
+++ resources/views/bio/index.blade.php
@@ -10,7 +10,7 @@
         <div class="row">
             <div class="col-md-3">
                 <center>
-                    <img src="{{ $member->photo }}" class="teamPic rounded-circle" alt=""><br><br>
+                    <img src="{{ $member->photo }}" class="teamPic " style="width: 128px;height: 128px;object-fit:cover;border-radius: 100px;"><br><br>
                     <span class="socialIcons">
                         <a href="{{ $member->facebook ?? '#' }}"><i class="fab fa-facebook"></i></a>&nbsp;
                         <a href="{{ $member->twitter ?? '#' }}"><i class="fab fa-twitter"></i></a>&nbsp;
@@ -31,14 +31,14 @@
 </div>
 
 <div class="container-fluid">
-    <div class="teamOthers">
+    <div class="teamOthers" style="background:#fafafa;">
         <div class="container">
             <h1>Learn More About Others</h1><br>
             <div class="row">
                 @foreach($team as $m)
                     <div class="col-md-3">
                         <Center>
-                            <img src="{{ $m->photo }}" class="teamPic"><br><br>
+                            <img src="{{ $m->photo }}" class="teamPic" style="width: 128px;height: 128px;object-fit:cover;border-radius: 100px;"><br><br>
                             <h6>{{ $m->name }}</h6>
                             <a href="{{ route('bio', $m) }}" class="bioLink">View Biography</a>
                         </Center>
--- resources/views/layouts/partials/team.blade.php
+++ resources/views/layouts/partials/team.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-4">
             <div class="team">
                 <center>
-                    <img src="{{ $member->photo }}" class="teamPic rounded-circle"><br><br>
+                    <img src="{{ $member->photo }}" class="teamPic img-rounded" style="width: 128px;height: 128px;object-fit:cover;border-radius: 100px;"><br><br>
                     <h4>{{ $member->name }}</h4>
                     <span class="socialIcons">
                         <i class="fab fa-facebook"></i>&nbsp;
