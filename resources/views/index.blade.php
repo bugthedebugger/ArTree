3b37aa15884ff14315ad788d58d49dc6d98ab3e0
AakashRajDahal <akashrajdahal@gmail.com>
2020-07-05 20:30:19 +0545

    Cards Redesgin

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -81,7 +81,7 @@
 <div class="container">
     <br /><br />
     <div class="news">
-        <h1>News and Updates</h1>
+        <h1>Events and Projects</h1>
         <br />
         <div class="row">
             @if($latestEvent != null)
@@ -111,16 +111,6 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
                                 }}<br />
                                 <i class="fal fa-clock"></i>&nbsp;
                                 {{ Carbon\Carbon::parse($latestEvent->events->start_time)->format('h:i A') }}
@@ -139,47 +129,50 @@
             </div>
             @endif @foreach($projects as $project)
             <div class="col-md-4">
-                <div class="card" style="height: 100%;">
-                    <img
-                        class="card-img-top cardImg"
-                        src="{{ $project->featured ?? asset('images/eye.png') }}"
-                        alt="Card image"
-                    />
-                    <div class="card-body">
-                        <p class="card-title">
-                            <i class="fal fa-calendar"></i>&nbsp;
-                            {{ Carbon\Carbon::parse($project->project_date)->format('l jS \\of F Y') }}
-                        </p>
-                        <p class="card-text">
-                            <strong>{{ $project->name }}</strong> <br />
-                            ...
-                            {{ substr(strip_tags($project->body), 10, 150) }}
-                            ...
-                        </p>
-                        <a
-                            href="{{ route('projects-read', ['year' => $project->projectYear->year, 'project' => $project]) }}"
-                            class="btn btn-artee"
-                            >Read More</a
-                        >
+                <a
+                    href="{{ route('projects-read', ['year' => $project->projectYear->year, 'project' => $project]) }}"
+                    class="cardLink"
+                >
+                    <div class="card" style="height: 100%;">
+                        <img
+                            class="card-img-top cardImg"
+                            src="{{ $project->featured ?? asset('images/eye.png') }}"
+                            alt="Card image"
+                        />
+                        <div class="card-body">
+                            <p class="card-title">
+                                <i class="fal fa-calendar"></i>&nbsp;
+                                {{ Carbon\Carbon::parse($project->project_date)->format('l jS \\of F Y') }}
+                            </p>
+                            <p class="card-text">
+                                <strong>{{ $project->name }}</strong> <br />
+                                {{ substr(strip_tags($project->body), 10, 150) }}
+                                ...
+                            </p>
+                        </div>
                     </div>
-                </div>
+                </a>
             </div>
             @endforeach
         </div>
     </div>
     <br /><br />
     <div class="news">
+        <h1>News media</h1>
+        <br />
+    </div>
+    <div class="news">
         <h1>Social Feeds</h1>
         <br />
         <div class="row">
             <div class="col-md-6">
                 <h5 style="float: left;">Instagram</h5>
-                <a
+                <!-- <a
                     class="btn btn-artee"
                     href="{{ $settings->instagram ?? '#' }}"
                     style="float: right;"
                     >Follow</a
-                ><br /><br />
+                ><br /><br /> -->
                 <!-- SnapWidget -->
                 <script src="https://snapwidget.com/js/snapwidget.js"></script>
                 <iframe
@@ -194,12 +187,12 @@
             </div>
             <div class="col-md-6">
                 <h5 style="float: left;">Facebook</h5>
-                <a
+                <!-- <a
                     class="btn btn-artee"
                     href="{{ $settings->facebook ?? '#' }}"
                     style="float: right;"
                     >Like</a
-                ><br /><br />
+                ><br /><br /> -->
                 <div class="text-center">
                     <iframe
                         src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FArtree-Nepal-295759690592034%2F&tabs=timeline&width=340&height=540&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=138713336895364"
