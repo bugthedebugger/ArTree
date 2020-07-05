f06449179dc1ea30a6734e3c82f64ede7661311a
AakashRajDahal <akashrajdahal@gmail.com>
2020-07-05 21:24:36 +0545

    Blog Page

--- resources/views/projects/blog.blade.php
+++ resources/views/projects/blog.blade.php
@@ -1,13 +1,67 @@
-@extends('layouts.artree')
-
-@section('title')
-Projects | {{ $project->projectYear->year }} | {{ $project->name }}
-@endsection
-
-@section('body')
+@extends('layouts.artree') @section('title') Projects |
+{{ $project->projectYear->year }} | {{ $project->name }}
+@endsection @section('body')
 <div class="container">
     <div class="page">
-        <div class="row">
+        <div class="blogHeader">
+            <h1 class="blogTitle">
+                {{ $project->name }}
+            </h1>
+        </div>
+        <div class="blogInfo">
+            {{ Carbon\Carbon::parse($project->project_date)->format('l jS \\of F Y') }}
+            , {{ $project->location }},
+            {{ round(strlen(strip_tags($project->body))/200) }} min read
+        </div>
+        <br />
+        <!-- Carousel -->
+        <center>
+            <div>
+                <div
+                    id="demo"
+                    class="carousel slide blogCaro"
+                    data-ride="carousel"
+                >
+                    <!-- The slideshow -->
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img
+                                src="{{ $project->featured ?? asset('images/project.png') }}"
+                                class="d-block w-100"
+                                style="object-fit: cover;"
+                            />
+                        </div>
+                        @foreach($project->gallery as $gallery)
+                        <div class="carousel-item">
+                            <img
+                                src="{{ $gallery->path }}"
+                                class="d-block w-100 img-fluid"
+                            />
+                        </div>
+                        @endforeach
+                    </div>
+
+                    <!-- Left and right controls -->
+                    <a
+                        class="carousel-control-prev"
+                        href="#demo"
+                        data-slide="prev"
+                    >
+                        <span class="far fa-arrow-left"></span>
+                    </a>
+                    <a
+                        class="carousel-control-next"
+                        href="#demo"
+                        data-slide="next"
+                    >
+                        <span class="far fa-arrow-right"></span>
+                    </a>
+                </div>
+                <br />
+            </div>
+        </center>
+
+        <!-- <div class="row">
             <div class="col-md-4">
                 <div>
                     <h1 class="blogTitle">{{ $project->name }} - {{ $project->projectYear->year }}</h1>
@@ -33,7 +87,7 @@ Projects | {{ $project->projectYear->year }} | {{ $project->name }}
                             "Although throughout the history, Nepal 's one of the best exports have been garments and textiles , general Nepali people started to loose their connection with it." 
                         </p>
                     </div>-->
-                    <div class="blogBookmark">
+        <!-- <div class="blogBookmark">
                         <h6>Like what you see?</h6>
                         <div>
                             <i class="fal fa-bookmark share"></i>
@@ -47,7 +101,7 @@ Projects | {{ $project->projectYear->year }} | {{ $project->name }}
                 <div>
                     <div id="demo" class="carousel slide arteeCaro" data-ride="carousel">       
                         <!-- The slideshow -->
-                        <div class="carousel-inner">
+        <!-- <div class="carousel-inner">
                             <div class="carousel-item active">
                                 <img src="{{ $project->featured ?? asset('images/project.png') }}" class="d-block w-100 img-fluid">
                             </div>
@@ -59,7 +113,7 @@ Projects | {{ $project->projectYear->year }} | {{ $project->name }}
                         </div>
                         
                         <!-- Left and right controls -->
-                        <a class="carousel-control-prev" href="#demo" data-slide="prev">
+        <!-- <a class="carousel-control-prev" href="#demo" data-slide="prev">
                             <span class="fal fa-arrow-left"></span>
                         </a>
                         <a class="carousel-control-next" href="#demo" data-slide="next">
@@ -68,16 +122,24 @@ Projects | {{ $project->projectYear->year }} | {{ $project->name }}
                     </div><br>
                 </div>
             </div>
-        </div>
-        <br><br>
+        </div> -->
+        <br /><br />
         <div class="row">
-            <div class="col-md-4">
-                
-            </div>
-            <div class="col-md-8">
-                <div style="text-align:justify">
+            <div class="col-md-12">
+                <div style="text-align: justify;" class="blogText">
                     {!! $project->body !!}
                 </div>
+            </div>
+        </div>
+        <hr />
+        <br />
+        <div style="max-width: 720px; margin-right: auto; margin-left: auto;">
+            <h6 style="font-family: Roboto; font-weight: bold;">
+                Share this Article
+            </h6>
+            <div>
+                <i class="fab fa-facebook-f share" style="color: #3b5998;"></i>
+                <i class="fab fa-twitter share" style="color: #1da1f2;"></i>
             </div>
         </div>
     </div>
