b1e867b9d639e4a3374903a052f67f9ad78e1ba3
markaa10 <rahulrajdahal@gmail.com>
2020-07-06 12:31:33 +0545

    Slider backend

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -8,7 +8,9 @@ use App\Models\Project;
 class LandingPageController extends Controller
 {
     public function index() {
-        $featuredProject = Project::where('hidden', false)->orderBy('project_date', 'desc')->first();
+        $featuredProjectOne = Project::where('hidden', false)->orderBy('project_date', 'desc')->first();
+        $featuredProjectTwo = Project::where('hidden', false)->orderBy('project_date', 'desc')->skip(1)->take(1)->first();
+        $featuredProjectThree = Project::where('hidden', false)->orderBy('project_date', 'desc')->skip(2)->take(1)->first();
         $projects = Project::where('hidden', false)->orderBy('project_date', 'desc')->take(4)->get();
         $latestEvent = Project::orderBy('project_date', 'desc')
                                 ->where([
@@ -17,7 +19,9 @@ class LandingPageController extends Controller
                                 ])
                                 ->first();
         return view('index')
-                ->with('featuredProject', $featuredProject)
+                ->with('featuredProjectOne', $featuredProjectOne)
+                ->with('featuredProjectTwo', $featuredProjectTwo)
+                ->with('featuredProjectThree', $featuredProjectThree)
                 ->with('projects', $projects)
                 ->with('latestEvent', $latestEvent);
     }
